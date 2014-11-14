43d67cf78d101d981963fea994c855ac5204a1c3
katiepark <katie.e.park@gmail.com>
2014-11-14 13:58:53 -0500

    Updating to v0.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='whippersnapper',
-    version='0.0.0',
+    version='0.0.1',
     author='Kevin Schaul, Katie Park',
     author_email='kevin.schaul@washpost.com, katie.park@washpost.com',
     url='http://github.com/washingtonpost/whippersnapper',
