3a0e4e37dda450d3736b785edcfbdb06bb10c982
katiepark <katie.e.park@gmail.com>
2014-11-14 14:58:16 -0500

    Update sample config file to just use github URL

--- whippersnapper/whippersnapper.py
+++ whippersnapper/whippersnapper.py
@@ -113,9 +113,7 @@ To quit, press ^C (ctrl-C).""" % (self.log_file)
         return config
 
     def usage(self):
-        config_template_file = os.path.abspath(os.path.expanduser(
-                os.path.abspath(os.path.dirname(inspect.getfile(inspect.currentframe())))
-                + '/../config_templates/config.yaml.template'))
+        config_template_file = 'https://raw.githubusercontent.com/washingtonpost/whippersnapper/master/config_templates/config.yaml.template'
 
         """
         Print usage information.
@@ -125,7 +123,7 @@ To quit, press ^C (ctrl-C).""" % (self.log_file)
 
         To see an example config file:
 
-        cat %s
+        curl %s
         """ % config_template_file
 
 def launch_new_instance():
