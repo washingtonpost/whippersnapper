15f53bf99d5ea7203831abed762581e90531e853
Kevin Schaul <kevin.schaul@gmail.com>
2014-11-13 23:15:13 -0500

    Set all defaults when loading config

--- elections_screenshotter/elections_screenshotter.py
+++ elections_screenshotter/elections_screenshotter.py
@@ -25,7 +25,7 @@ class ElectionsScreenshotter(object):
         self.config = self.load_config(config_filepath)
         self.log_file = self.init_logging()
         self.screenshotter = screenshotter.Screenshotter(self.config)
-        if not self.config.get('skip_upload', False):
+        if not self.config.get('skip_upload'):
             self.uploader = uploader.Uploader(self.config)
 
     def main(self):
@@ -44,19 +44,17 @@ To quit, press ^C (ctrl-C).""" % (self.log_file)
             images = self.screenshotter.take_screenshots()
             try:
                 filepaths = self.uploader.upload_screenshots(images)
-                if (self.config.get('delete_local_images', False)):
+                if (self.config.get('delete_local_images')):
                     [os.remove(path.get('local_filepath')) for path in images]
             except AttributeError:
                 pass
-            time.sleep(self.config.get('time_between_screenshots', 60))
+            time.sleep(self.config.get('time_between_screenshots'))
 
     def init_logging(self):
         """
         Create a log file, and attach a basic logger to it.
         """
-        log_default = os.path.dirname(os.path.abspath(__file__)) + '/../screenshotter.log'
-        log_file = self.config.get('log_file',
-                log_default)
+        log_file = self.config.get('log_file')
         # Create the log file if it does not yet exist
         with open(log_file, 'a+'):
             pass
@@ -72,8 +70,42 @@ To quit, press ^C (ctrl-C).""" % (self.log_file)
         This file includes the urls, css selectors and slugs for the images to
         screenshot.
         """
+        config = {
+            'skip_upload': False,
+            'aws_bucket': '',
+            'aws_subpath': '',
+            'aws_access_key': None,
+            'aws_secret_key': None,
+            'log_file': os.path.dirname(os.path.abspath(__file__)) +
+                   '/../screenshotter.log',
+            'delete_local_images': False,
+            'time_between_screenshots': 60,
+            'override_css_file': None,
+            'page_load_delay': 2,
+            'wait_for_js_signal': False,
+            'failure_timeout': 30,
+        }
+
+        required = (
+            'images',
+            'local_image_directory',
+        )
+
+        raw_config = None
+
         with open(config_filepath) as f:
-            return yaml.load(f)
+            raw_config = yaml.load(f)
+
+        for option_name, option_value in raw_config.iteritems():
+            config[option_name] = option_value
+
+        for option in required:
+            try:
+                config[option] = raw_config[option]
+            except KeyError:
+                raise RuntimeError('Config does not have required attribute: %s' % option)
+
+        return config
 
     def usage(self):
         """
